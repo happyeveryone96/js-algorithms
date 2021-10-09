178a226d6d70b7bc306001bf73ac33923159adaa
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2021-10-09 17:15:57 +0900

    A+B - 7

---
+++ baekjoon/3단계/11021.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split('\n');
+
+for (let i=1; i <= input[0]; i++) {
+    let num = input[i].split(' ');
+    let a = parseInt(num[0]);
+    let b = parseInt(num[1]);
+    console.log(`Case #${i}: ${a+b}`)
+}
