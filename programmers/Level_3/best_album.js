abf0bd6149389096213a1d838fd0a0ea7d76ad8c
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-02-09 20:51:55 +0900

    베스트앨범

---
+++ programmers/Level_3/best_album.js
@@ -0,0 +1,24 @@
+// 1. 같은 장르끼리 묶는다.
+// 2. 묶인 노래들을 재생순으로 정렬한다.
+// 3. 노래를 2개까지 자르는 작업
+
+// 핵심 키워드는 "묶는 것", "정렬"
+
+function solution(genres, plays) {
+  const genreMap = new Map();
+  genres
+      .map((genre, index) => [genre, plays[index]])
+      .forEach(([genre, play], index) => {
+          const data = genreMap.get(genre) || { total: 0, songs: [] };
+          genreMap.set(genre, {
+              total: data.total + play,
+              songs: [...data.songs, { play, index }]
+                  .sort((a,b) => b.play - a.play)
+                  .slice(0,2)
+          })
+      })
+  return [...genreMap.entries()]
+      .sort((a,b) => b[1].total - a[1].total)
+      .flatMap(item => item[1].songs)
+      .map(song => song.index)
+}
