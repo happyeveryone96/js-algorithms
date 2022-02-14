555d10a30e6a9283b37cfbd292f7754fb8d49848
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-02-14 21:13:32 +0900

    주사위 세개

---
+++ baekjoon/2단계/2480.js
@@ -0,0 +1,20 @@
+const fs = require('fs'); 
+const input = fs.readFileSync("/dev/stdin").toString().split(' ');
+
+const firstNum = parseInt(input[0]);
+const secondNum = parseInt(input[1]);
+const thirdNum = parseInt(input[2]);
+
+if (firstNum === secondNum && secondNum === thirdNum) {
+    console.log(10000 + firstNum * 1000);
+} else if (firstNum === secondNum && secondNum !== thirdNum) {
+    console.log(1000 + firstNum * 100);
+} else if (firstNum === thirdNum && secondNum !== thirdNum) {
+    console.log(1000 + firstNum * 100);
+} else if (secondNum === thirdNum && firstNum !== thirdNum) {
+    console.log(1000 + secondNum * 100);
+} else if (firstNum !== secondNum && secondNum !== thirdNum) {
+    const arr = [firstNum, secondNum, thirdNum];
+    const sortedArr = arr.sort((a,b) => a - b);
+    console.log(sortedArr[2] * 100);
+}
