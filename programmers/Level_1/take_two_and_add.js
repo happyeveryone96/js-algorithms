2b4a6c78bfe981724e633c2b0055248bf87791ae
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-02-20 20:14:46 +0900

    두 개 뽑아서 더하기

--- programmers/Level_1/take_two_and_add.js
+++ programmers/Level_1/take_two_and_add.js
@@ -1,15 +1,38 @@
+// 1. 이중 반복문 이용
 function solution(numbers) {
-    var answer = [];
+    const arr = [];
     for (let i = 0; i < numbers.length; i++) {
         for (let j = 0; j < numbers.length; j++) {
             if (i !== j) {
-                answer.push(numbers[i]+numbers[j]);
+                arr.push(numbers[i]+numbers[j]);
             }
         }
     }
-    let result = answer.filter((element, index) => {
-        return answer.indexOf(element) === index;
+    let answer = arr.filter((element, index) => {
+        return arr.indexOf(element) === index;
     });
-    result = result.sort((a,b) => a-b);
-    return result;
+    answer = answer.sort((a,b) => a - b);
+    return answer;
 }
+
+// 2. 조합 이용
+function combinations(arr, n) {
+    if (n === 1) return arr.map((v) => [v]);
+    const result = [];
+  
+    arr.forEach((fixed, idx, arr) => {
+      const rest = arr.slice(idx + 1);
+      const combis = combinations(rest, n - 1);
+      const combine = combis.map((v) => [fixed, ...v]);
+      result.push(...combine);
+    });
+    return result;
+  }
+  
+  function solution(numbers) {
+    // 1. 조합을 구한다. n 개중 2개
+    // 2. 조합의 합을 구한다.
+    // 3. 중복을 제거한다.
+    // 4. 오름차순 정렬한다.
+    return [...new Set(combinations(numbers, 2).map(combi => combi[0] + combi[1]))].sort((a, b) => a - b);
+  }
