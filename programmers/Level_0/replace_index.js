22a766c3f6e291dab0a6c315945628d4dc6b7293
happyeveryone96 <happyeveryone@naver.com>
2023-02-21 23:37:10 +0900

    인덱스 바꾸기

--- programmers/Level_0/replace_index.js
+++ programmers/Level_0/replace_index.js
@@ -1,9 +1,14 @@
 function solution(my_string, num1, num2) {
-  var answer = [];
-  for (let i = 0; i < my_string.length; i++) {
-    if (i === num1) answer.push(my_string[num2]);
-    else if (i === num2) answer.push(my_string[num1]);
-    else answer.push(my_string[i]);
-  }
-  return answer.join("");
+  // var answer = [];
+  // for (let i = 0; i < my_string.length; i++) {
+  //   if (i === num1) answer.push(my_string[num2]);
+  //   else if (i === num2) answer.push(my_string[num1]);
+  //   else answer.push(my_string[i]);
+  // }
+  // return answer.join("");
+  const str = my_string.split("");
+  const tmp = str[num1];
+  str[num1] = str[num2];
+  str[num2] = tmp;
+  return str.join("");
 }
